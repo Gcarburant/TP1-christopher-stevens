6747e4af68c9c61f5434163ebc995ed4a1b0fa7c
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-03-28 14:59:00 -0400

    categorie

--- category.php
+++ category.php
@@ -4,32 +4,29 @@
     <section class="formation">
         <?php wp_nav_menu(array(
             "menu" => 'categorie_cours',
-            "container" => 'lab'
+            "container" => "nav"
             )); ?>
         <h2 class="formation__titre">Liste des cours du programme TIM</h2>
+        <?php
+        if (is_category('web')){
+            // $ma_categorie = get_the_category_by_slug('web');
+
+        }
+
+            if(is_category('web')){echo "<h3>Cours web</h3>";}
+            if(is_category('jeu')){echo "<h3>Cours design</h3>";}
+            if(is_category('design')){echo "<h3>Cours itilitaire</h3>";}
+            if(is_category('utilitaire')){echo "<h3>Cours web</h3>";}
+            if(is_category('creation-3d')){echo "<h3>Cours web</h3>";}
+            if(is_category('video')){echo "<h3>Cours web</h3>";}
+            $url_categorie = trouve_la_categorie(array('web','jeu','design', 'utilistaire','creation-3d', 'video'));
+            $ma_categorie
+        ?>
         <div class="formation__liste">
             <?php if (have_posts()):
                 while (have_posts()): the_post(); ?>
-            <article class="formation__cours">
-                <?php
-              
-                $titre = get_the_title();
-                $titreFiltreCours = substr($titre, 7, -6);
-                $nbHeures = substr($titre, -6);
-                $sigleCours = substr($titre, 0, 7);
-                $descCours = get_the_excerpt();
-                ?>
-                <?php the_post_thumbnail("thumbnail"); ?>
-                <h3 class="cours__titre">
-                    <a href="<?php echo get_permalink(); ?>">
-                        <?= $titreFiltreCours; ?>
-                    </a>
-                </h3>
-                <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
-                <p class="cours__sigle"><?= $sigleCours; ?> </p>
-                <p class="cours__desc"> <?= $descCours; ?></p>
-
-            </article>
+                <?php get_template_part("gabarits/content","cours"); ?>
+            
             <?php endwhile ?>
             <?php endif ?>
         </div>
